5e239703e8a22ebc6109d1fd90160b2375a630dd
Jesse Bounds <jesse@rebounds.net>
2016-09-07 11:12:11 -0700

    [ios, macos] Remove commented code

--- platform/darwin/src/MGLStyleLayer.h
+++ platform/darwin/src/MGLStyleLayer.h
@@ -9,8 +9,4 @@
 @property (nonatomic, readonly) NSString *sourceIdentifier;
 @property (nonatomic, readonly) NSString *sourceLayerIdentifier;
 
-//- (instancetype)initWithLayerIdentifier:(NSString *)layerIdentifier;
-//- (instancetype)initWithLayerIdentifier:(NSString *)layerIdentifier sourceIdentifier:(NSString *)sourceIdentifier;
-//- (instancetype)initWithLayerIdentifier:(NSString *)layerIdentifier sourceIdentifier:(NSString *)sourceIdentifier sourceLayer:(NSString *)sourceLayer;
-
 @end
